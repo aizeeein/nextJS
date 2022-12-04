351222078d667b97b2c63b38a2e4b2d5e28e23f3
unknown <zein.abdullah12@gmail.com>
2022-12-04 15:14:46 +0700

    fix1

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import MediumCard from "../components/MediumCard";
 import LargeCard from "../components/LargeCard";
 import Footer from "../components/Footer";
 
-export default function Home({ exploreData, cardsData }) {
+export default function Home({ exploreData, cardsData }: any) {
   return (
     <div className="">
       <Head>
@@ -24,7 +24,7 @@ export default function Home({ exploreData, cardsData }) {
 
           {/* Pull some data */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData?.map(({ img, distance, location }) => (
+            {exploreData?.map(({ img, distance, location }: any) => (
               <SmallCard
                 key={img}
                 img={img}
@@ -37,16 +37,16 @@ export default function Home({ exploreData, cardsData }) {
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
-            {cardsData?.map(({ img, title }) => (
+            {cardsData?.map(({ img, title }: any) => (
               <MediumCard key={img} img={img} title={title} />
             ))}
           </div>
         </section>
-        <LargeCard 
-        img="https://links.papareact.com/4cj"
-        title='The Greatest Outdoors'
-        description="Wishlists curated by Airbnb."
-        buttonText="Get Inspired"
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get Inspired"
         />
       </main>
       <Footer />
