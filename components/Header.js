50b03e11030d82456c8a6325b9aca4aed71ced31
unknown <zein.abdullah12@gmail.com>
2022-12-12 22:55:01 +0700

    tenth

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,13 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { GlobeAltIcon } from "@heroicons/react/24/solid";
 
-function Header() {
+function Header({ placeholder }) {
+  const [noOfGuests, setNoOfGuests] = useState(1);
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const router = useRouter();
+
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
@@ -21,19 +27,23 @@ function Header() {
   const resetInput = () => {
     setSearchInput("");
   };
-  const [numberOfGuests, setNumberOfGuests] = useState(1);
-  const [searchInput, setSearchInput] = useState("");
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const router = useRouter();
 
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
     key: "selection",
   };
-  console.log(searchInput);
-
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
@@ -55,7 +65,7 @@ function Header() {
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
-          placeholder="Start Your Search!"
+          placeholder={placeholder || "Start Your Search!"}
           className="flex-grow pl-5 bg-transparent outline-none"
         />
         <MagnifyingGlassIcon className="hidden lg:inline-flex h-8 cursor-pointer bg-red-400 rounded-full p-2 text-white md:mx-2" />
@@ -63,8 +73,11 @@ function Header() {
 
       {/* Right */}
       <div className="flex items-center space-x-4 justify-end text-gray-500">
-        <p className="hidden md:inline">Become a host</p>
-        <GlobeAltIcon onClick={() => router.push('search')} className="h-6 cursor-pointer" />
+        <p className="hidden md:inline">3:49:21</p>
+        <GlobeAltIcon
+          onClick={() => router.push("search")}
+          className="h-6 cursor-pointer"
+        />
         <div className="flex items-center space-x-2 border-2 rounded-full p-2">
           <Bars3Icon className="h-6 cursor-pointer" />
           <UserCircleIcon className="h-6 cursor-pointer" />
@@ -84,8 +97,8 @@ function Header() {
             </h2>
             <UsersIcon className="h-5" />
             <input
-              value={numberOfGuests}
-              onChange={(e) => setNumberOfGuests(e.target.value)}
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
               type="number"
               min={1}
               className="w-12 pl-2 text-lg outline-none text-red-400"
@@ -95,7 +108,9 @@ function Header() {
             <button onClick={resetInput} className="flex-grow text-gray-500">
               Cancel
             </button>
-            <button className="flex-grow text-red-400">Search</button>
+            <button onClick={search} className="flex-grow text-red-400">
+              Search
+            </button>
           </div>
         </div>
       )}
